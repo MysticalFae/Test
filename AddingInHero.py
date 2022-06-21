bbfc19fe20a8711e9ca1841373910c0084ac936f
MysticalFae <2004jellyfishart@gmail.com>
2022-06-21 10:20:23 -0500

    turtle moves from left to right

--- AddingInHero.py
+++ AddingInHero.py
@@ -7,18 +7,18 @@ class Character:
     def __init__(self, screen):
         self.screen = screen
 
-        self.charImage = pygame.image.load("turtle-png-28.png")
+        self.charImage = pygame.image.load("turtle-png-28.png")  #brings in photo
 
-        self.x = screen.get_width() / 2 - self.charImage.get_width() / 2
-        self.y = screen.get_height() / 2 - self.charImage.get_height() / 2
+        self.x = screen.get_width() / 2 - self.charImage.get_width() / 2  # centers in on x-axis
+        self.y = screen.get_height() - self.charImage.get_height()  # brings it to bot of y without going off screen
 
 
-    def move(self, x):
-        self.x = x
+    def move(self, newX):
+        self.x = self.x + newX
         pass
 
     def draw(self):
-        self.screen.blit(self.charImage, (self.x, self.y))
+        self.screen.blit(self.charImage, (self.x, self.y))  # prints image at the x y cords set in init
 
 
 
@@ -42,11 +42,11 @@ def main():
         screen.blit(background, (0, 0))
 
 
-        # pressedKey = pygame.key.get_pressed()
-        # if pressedKey[pygame.K_LEFT]:
-        #     character1.move(-5)
-        # if pressedKey[pygame.K_RIGHT]:
-        #     character1.move(5)
+        pressedKey = pygame.key.get_pressed()
+        if pressedKey[pygame.K_LEFT]:
+            character1.move(-5)
+        if pressedKey[pygame.K_RIGHT]:
+            character1.move(5)
 
         character1.draw()
 
