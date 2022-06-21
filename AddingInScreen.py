12d97a54251522ada897da4ade5e3f8e89b22de6
MysticalFae <2004jellyfishart@gmail.com>
2022-06-21 09:59:07 -0500

    successfully added background image and re-sized everything

--- AddingInScreen.py
+++ AddingInScreen.py
@@ -6,17 +6,19 @@ import time
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((500, 500))  #touple
-    pygame.display.set_caption("Endless something")
+    screen = pygame.display.set_mode((800, 700))  #touple
+    pygame.display.set_caption("Endless image")
     background = pygame.image.load("backgroundImage.png")
+    background = pygame.transform.scale(background, (800, 700))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+
         screen.blit(background, (0, 0))
 
-
+        pygame.display.update()
 
 main()
